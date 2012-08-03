856650d09621a50d4b2beb30c98bb023600ce7f6
Roland Keller <rolandkeller5@googlemail.com>
2012-08-03 13:28:57 +0000

    Some improvements.

--- src/org/sbml/squeezer/sabiork/util/XMLParser.java
+++ src/org/sbml/squeezer/sabiork/util/XMLParser.java
@@ -1,3 +1,26 @@
+/*
+ * $$Id${file_name} ${time} ${user} $$
+ * $$URL${file_name} $$
+ * ---------------------------------------------------------------------
+ * This file is part of SBMLsqueezer, a Java program that creates rate 
+ * equations for reactions in SBML files (http://sbml.org).
+ *
+ * Copyright (C) 2006-2012 by the University of Tuebingen, Germany.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * ---------------------------------------------------------------------
+ */
 package org.sbml.squeezer.sabiork.util;
 
 import java.io.ByteArrayInputStream;
@@ -12,6 +35,11 @@ import javax.xml.stream.events.Characters;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 
+/**
+ * @author Matthias Rall
+ * @version $$Rev$$
+ * ${tags}
+ */
 public class XMLParser {
 
 	public static List<String> getMultipleElementTextContent(String xml, String elementQNameLocalPart, String elementQNameNamespaceURI) throws XMLStreamException, UnsupportedEncodingException {
