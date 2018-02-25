68050b9be3f15c405d99062c3d3c969a732e565e
lisa <lisa.falk@student.uni-tuebingen.de>
2018-02-25 13:50:23 +0100

    added package FunctionTermGenerator

---
+++ src/org/sbml/squeezer/functionTermGenerator/FunctionTermOptions.java
@@ -0,0 +1,20 @@
+package org.sbml.squeezer.functionTermGenerator;
+
+import java.util.ResourceBundle;
+
+import org.sbml.jsbml.ext.qual.Sign;
+import org.sbml.squeezer.util.Bundles;
+
+import de.zbit.util.ResourceManager;
+import de.zbit.util.prefs.KeyProvider;
+import de.zbit.util.prefs.Option;
+
+public interface FunctionTermOptions extends KeyProvider{
+
+	/**
+	 * Localization support
+	 */
+	public static final ResourceBundle OPTIONS_BUNDLE = ResourceManager.getBundle(Bundles.OPTIONS);
+
+	public static final Option<Sign> DEFAULT_SIGN = new Option<>("DEFAULT_SIGN", Sign.class, OPTIONS_BUNDLE, Sign.positive);
+}
